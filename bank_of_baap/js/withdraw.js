491ec8a28cf96eb6ccd1cdc0cfc98fd2a50d6f9d
Dipu Haider <dipuhaider@gmail.com>
2023-08-15 17:00:56 +0600

    Update withdraw.js

--- bank_of_baap/js/withdraw.js
+++ bank_of_baap/js/withdraw.js
@@ -3,22 +3,35 @@ document.getElementById("withdraw-btn").addEventListener("click", function () {
   const withdrawField = document.getElementById("withdraw-field");
   const newWithdrawAmountString = withdrawField.value;
   const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+  //   clear the withdraw field
+  withdrawField.value = "";
+
+  if (isNaN(newWithdrawAmount)) {
+    alert("please provide a valid number");
+    return;
+  }
+
   const withdrawTotalElement = document.getElementById("withdraw-total");
   const previousWithdrawTotalString = withdrawTotalElement.innerText;
   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-
-  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-
-  withdrawTotalElement.innerText = currentWithdrawTotal;
 
   const balanceTotalElement = document.getElementById("balance-total");
   const previousBalanceTotalString = balanceTotalElement.innerText;
   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+  //   clear the withdraw field
+  withdrawField.value = "";
+
+  if (newWithdrawAmount > previousBalanceTotal) {
+    alert("Balance not enough");
+    return;
+  }
+
+  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  withdrawTotalElement.innerText = currentWithdrawTotal;
+
   const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
 
   balanceTotalElement.innerText = currentBalanceTotal;
-
-  //   clear the withdraw field
-  withdrawField.value = "";
 });
