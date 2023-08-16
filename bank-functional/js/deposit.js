b3b8c92e8a9d4da2691ea653c6f338d026167e0c
Dipu Haider <dipuhaider@gmail.com>
2023-08-16 16:55:04 +0600

    update

--- bank-functional/js/deposit.js
+++ bank-functional/js/deposit.js
@@ -8,35 +8,42 @@
 6. get current balance
 7. calculate the new balance and set the value to the balance total element
 */
+
 // step 1
 document.getElementById("deposit-btn").addEventListener("click", function () {
   //console.log("clicked");
 
-  // step 2
-  const depositField = document.getElementById("deposit-field");
-  const newDepositAmountString = depositField.value;
+  //   // step 2
+  //   const depositField = document.getElementById("deposit-field");
+  //   const newDepositAmountString = depositField.value;
+  //   // Step 2.5
+  //   const newDepositAmount = parseFloat(newDepositAmountString);
+  //   // step 3
+  //   depositField.value = "";
 
-  // Step 2.5
-  const newDepositAmount = parseFloat(newDepositAmountString);
+  const newDepositAmount = getInputFieldValueById("deposit-field");
 
-  // step 3
-  depositField.value = "";
+  //   // step 4
+  //   const depositTotalElement = document.getElementById("deposit-total");
+  //   const previousDepositTotalString = depositTotalElement.innerText;
+  //   const previousDepositTotal = parseFloat(previousDepositTotalString);
 
-  // step 4
-  const depositTotalElement = document.getElementById("deposit-total");
-  const previousDepositTotalString = depositTotalElement.innerText;
-  const previousDepositTotal = parseFloat(previousDepositTotalString);
+  const previousDepositTotal = getTextElementValueById("deposit-total");
 
   // step 5
-  const currentDepositTotal = previousDepositTotal + newDepositAmount;
-  depositTotalElement.innerText = currentDepositTotal;
+  const newDepositTotal = previousDepositTotal + newDepositAmount;
 
-  // step 6
-  const balanceTotalElement = document.getElementById("balance-total");
-  const previousBalanceTotalString = balanceTotalElement.innerText;
-  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+  //   depositTotalElement.innerText = newDepositTotal;
+  setTextElementValueById("deposit-total", newDepositTotal);
+
+  //   // step 6
+  //   const balanceTotalElement = document.getElementById("balance-total");
+  //   const previousBalanceTotalString = balanceTotalElement.innerText;
+  //   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+  const previousBalanceTotal = getTextElementValueById("balance-total");
 
   // step 7
-  const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
-  balanceTotalElement.innerText = currentBalanceTotal;
+  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+  //balanceTotalElement.innerText = newBalanceTotal;
+  setTextElementValueById("balance-total", newBalanceTotal);
 });
